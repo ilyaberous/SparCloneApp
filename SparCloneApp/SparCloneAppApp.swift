e730863a8035ce215ccb4dcc789b82369552d559
Ilya-Tatsenko <ilya.tatsenko@yandex.ru>
2023-08-13 14:12:16 +0300

    Initial Commit

---
+++ SparCloneApp/SparCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparCloneAppApp.swift
+//  SparCloneApp
+//
+//  Created by Ilya on 13.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SparCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
